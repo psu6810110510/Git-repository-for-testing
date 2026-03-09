8a4030d5079df0b3acb79911cf5e082c5100727d
นายศรสทธ ศภสาร <6810110510@psu.ac.th>
2026-03-10 01:02:21 +0700

    Add Test_FunnyHacker1 and Change name flie Funny

--- test_double (1)/test_double/hello.py
+++
@@ -1,6 +0,0 @@
-def hello_world():
-    print(1 + 1)
-
-
-if __name__ == "__main__":
-    hello_world()
---
+++ tests/test_FunnyHecker1.py
@@ -0,0 +1,25 @@
+from coe_6810110510.FunnyHecker1 import funnyString
+import unittest
+
+#ตามHeckerrank
+class FunnyHecker1TestCase(unittest.TestCase):
+    def test_funny_string_input_acxz_should_be_funny(self):
+        self.assertEqual(funnyString("acxz"), "Funny")
+
+    def test_funny_string_input_bmop_should_be_not_funny(self):
+        self.assertEqual(funnyString("bcxz"), "Not Funny")
+    
+    def test_funny_string_input_ivvkxq_should_be_not_funny(self):
+        self.assertEqual(funnyString("ivvkxq"), "Not Funny")
+
+    def test_funny_string_input_ivvkx_should_be_not_funny(self):
+        self.assertEqual(funnyString("ivvkx"), "Not Funny")
+
+
+
+#เพิ่มเติม
+    def test_funny_string_input_a_should_be_funny(self):
+        self.assertEqual(funnyString("a"), "Funny")
+
+    def test_funny_string_input_empty_should_be_funny(self):
+        self.assertEqual(funnyString(""), "Funny")
